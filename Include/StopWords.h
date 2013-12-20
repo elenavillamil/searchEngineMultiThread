516ede577250845bdecaefe2fea9325c5e2908bb
elenavillamil <mvillamilrod"smu.edu>
2013-12-19 21:33:44 -0600

    Starting project

---
+++ Include/StopWords.h
@@ -0,0 +1,17 @@
+// EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE //
+//                                                                              //
+//                                                                              //
+// EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE //
+
+#ifndef STOPWORDS_H
+#define STOPWORDS_H
+
+class StopWords
+{
+private:
+	int myNumber;
+public:
+	StopWords();
+};
+
+#endif 
